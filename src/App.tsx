4eb81750df376c5ccab0063c76cd84b9ecf17f91
VengerAlex <alexvenger90@gmail.com>
2022-10-10 14:56:22 +0300

    EXPA_PREPARATION: add new pages & create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React, { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
+import NotFound from "./pages/NotFound";
+import { ROUTES } from "./utils/types";
 
 const App: FC = () => {
-  return <div>start</div>;
+  return (
+    <Routes>
+      <Route path={ROUTES.Home} element={<Home />} />
+      <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+      <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+      <Route path={ROUTES.NotFound} element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;

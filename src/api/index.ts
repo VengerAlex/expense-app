fa54ab2ddf9f330d6dd97c14b66fdd9125c86dde
VengerAlex <alexvenger90@gmail.com>
2022-10-10 09:08:03 +0300

    EXPA_PREPARATION: create instance of axios

---
+++ src/api/index.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: { "Content-Type": "application/json" },
+});
+
+instance.interceptors.request.use((config: AxiosRequestConfig) => {
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers!.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default instance;
---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+export enum ROUTES {
+  Home = "/",
+  Auth = "/auth",
+  Denied = "/denied",
+  NotFound = "*",
+}
+
+export enum LOADING_STATUS {
+  PENDING = "PENDING",
+  FULFILLED = "FULFILLED",
+  REJECTED = "REJECTED",
+}

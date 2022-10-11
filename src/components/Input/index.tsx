f3a86a15051cffe109bbe97565c4fbc29380d8c3
VengerAlex <alexvenger90@gmail.com>
2022-10-11 16:33:46 +0300

    EXPA_PREPARATION ADD: images & styles.ts

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+
+import { IconButton, InputAdornment, TextFieldProps } from "@mui/material";
+import { StyledInput } from "../../styles";
+
+type IInput = TextFieldProps & {
+  isPassword?: boolean;
+  control: any;
+  formName: string;
+};
+
+const Input: FC<IInput> = ({
+  isPassword = false,
+  formName,
+  control,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <StyledInput
+      {...control.register(formName, { required: true, minLength: 6 })}
+      {...props}
+      name={formName}
+      type={!isPassword ? "text" : showPassword ? "text" : "password"}
+      variant="standard"
+      InputProps={
+        isPassword
+          ? {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    sx={{ color: "#fff" }}
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <RemoveRedEyeOutlinedIcon />
+                    ) : (
+                      <VisibilityOffOutlinedIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }
+          : null
+      }
+    />
+  );
+};
+
+export default Input;
---
+++ src/components/MyLink/index.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactNode } from "react";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+
+interface ILink {
+  to: string;
+  children: ReactNode;
+}
+
+export const MyLink: FC<ILink> = ({ to, children }) => {
+  return (
+    <Link to={to}>
+      <Typography
+        sx={{ color: "#69B0FF" }}
+        component="span"
+        variant="subtitle2"
+      >
+        {children}
+      </Typography>
+    </Link>
+  );
+};

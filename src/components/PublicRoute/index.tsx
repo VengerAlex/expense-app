a5daf8713f45e163aaddc45e1458cce2f852bcfd
VengerAlex <alexvenger90@gmail.com>
2022-10-17 13:05:00 +0300

    EXPA_AUTH UPD: public route

---
+++ src/components/PublicRoute/index.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { ROUTES } from "../../utils/types";
+import { localstorageAuthService } from "../../services/localstorage.service";
+
+export const PublicRoute = () => {
+  const { state } = useLocation();
+  const token = localstorageAuthService.getAccessToken();
+
+  const pathGoBack = state?.from.path || ROUTES.Home;
+
+  if (token) return <Navigate to={pathGoBack} />;
+
+  return <Outlet />;
+};

7a9b4bd379c74acd79f07866b35fa801dd94afe9
VengerAlex <alexvenger90@gmail.com>
2022-10-21 15:49:36 +0300

    EXPA_DEVELOP UPD: sign-up page

--- src/pages/Auth/Reset/index.tsx
+++ src/pages/Auth/Reset/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ReactElement, useState } from "react";
 import { AuthPageWrapper } from "../../../components/AuthPageWrapper";
 import { NotificationBox } from "../../../components/NotificationBox";
 import { RESET_PAGE, ROUTES } from "../../../utils/types";
@@ -7,27 +7,35 @@ import { ResetPassword } from "./ResetPassword";
 import signUpCover from "../../../assets/images/cover-sign-up.jpg";
 import signInCover from "../../../assets/images/cover-login.jpg";
 
+type ISteps = Record<RESET_PAGE, { component: ReactElement }>;
+
 const ResetPage = () => {
   const [currentComponent, setCurrentComponent] = useState(RESET_PAGE.EMAIL);
 
   const currentImage =
     currentComponent === RESET_PAGE.NOTIFICATION ? signInCover : signUpCover;
 
-  return (
-    <AuthPageWrapper bgImage={currentImage}>
-      {currentComponent === RESET_PAGE.EMAIL && (
-        <EmailReset setCurrentComponent={setCurrentComponent} />
-      )}
-      {currentComponent === RESET_PAGE.PASSWORDS && (
-        <ResetPassword setCurrentComponent={setCurrentComponent} />
-      )}
-      {currentComponent === RESET_PAGE.NOTIFICATION && (
+  const STEPS: ISteps = {
+    [RESET_PAGE.EMAIL]: {
+      component: <EmailReset setCurrentComponent={setCurrentComponent} />,
+    },
+    [RESET_PAGE.PASSWORDS]: {
+      component: <ResetPassword setCurrentComponent={setCurrentComponent} />,
+    },
+    [RESET_PAGE.NOTIFICATION]: {
+      component: (
         <NotificationBox
           navigateTo={ROUTES.SIGN_IN}
           btnTitle="Login"
           title="Your password has been successfully changed"
         />
-      )}
+      ),
+    },
+  };
+
+  return (
+    <AuthPageWrapper bgImage={currentImage}>
+      {STEPS[currentComponent].component}
     </AuthPageWrapper>
   );
 };

d3063e751d0aa3090ba4004b2393417b5bb54116
VengerAlex <alexvenger90@gmail.com>
2022-10-21 15:16:04 +0300

    EXPA_SETTINGS UPD: yup schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,5 +1,8 @@
 import * as yup from "yup";
 
+const SPACE_REGEXP = /^(.*)?\S+(.*)?$/;
+const SPACE_WARNING = "can't be empty";
+
 export const resetSchema = yup.object().shape({
   email: yup.string().email().required(),
 });
@@ -7,13 +10,13 @@ export const resetSchema = yup.object().shape({
 export const signInSchema = yup.object().shape({
   username: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "can't be empty")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(5)
     .max(30),
   password: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "can't be empty")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(8)
     .max(30),
@@ -22,7 +25,7 @@ export const signInSchema = yup.object().shape({
 export const resetPasswordSchema = yup.object().shape({
   password: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "can't be empty")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(8)
     .max(30),
@@ -34,13 +37,13 @@ export const resetPasswordSchema = yup.object().shape({
 export const signUpSchema = resetPasswordSchema.shape({
   username: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "can't be empty")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(5)
     .max(30),
   fullName: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "can't be empty")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(5)
     .max(30),
@@ -50,13 +53,13 @@ export const signUpSchema = resetPasswordSchema.shape({
 export const extendedSettingsSchema = yup.object().shape({
   username: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "can't be empty")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(5)
     .max(30),
   displayName: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "can't be empty")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(5)
     .max(30),
@@ -65,7 +68,7 @@ export const extendedSettingsSchema = yup.object().shape({
 export const profileSettingsSchema = resetPasswordSchema.shape({
   oldPassword: yup
     .string()
-    .matches(/^(.*)?\S+(.*)?$/, "")
+    .matches(SPACE_REGEXP, SPACE_WARNING)
     .required()
     .min(8)
     .max(30),
